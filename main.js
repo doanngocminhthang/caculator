bf8c6dce9c0fa68b67e3cefd25c7c78bb8bacc68
minhthang <doanngocminhthang@gmail.com>
2021-11-29 09:27:00 +0700

    Done save calcultion history function

--- main.js
+++ main.js
@@ -6,11 +6,6 @@ for (let number of numbers) {
    result.innerHTML +=  this.value 
 });
 }
-function equal() {
-    let res = result.innerHTML;
-    let output = eval(res);
-    result.innerHTML = output ;
-}
 
 function clean() {
     result.innerHTML = " ";
@@ -29,4 +24,37 @@ checkbox.addEventListener('change',function(){
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
     }
+})
+
+// Save calculation history
+const logHistory = document.getElementById("history"); // Get element với id được chỉ định 
+let history = []; //khai báo array empty
+let expressionData = ""; // khai báo hàm để lưu biểu thức
+let resultData = ""; // khai báo hàm để lưu kết quả tính toán
+
+function equal() {
+    let res = result.innerHTML;
+    let output = eval(res);
+    expressionData = res // gán biểu thức tính toán vào hàm đã được khai báo
+    resultData = output // gán kết quả tính toán vào hàm đã được khai báo
+
+    // push object gồm có key là expression, result lần lượt với những value được gán ở trên
+    history.push({expression : expressionData, result: resultData});
+    //gọi hàm showHistory
+    showHistory()
+    result.innerHTML = output 
+}
+
+function showHistory() {
+    let log = "" // khai báo hàm log
+    for(let key in history) {
+        log += history[key].expression + "=" + history[key].result + "<br>" 
+    }
+    logHistory.innerHTML = log // Hiển thị ra view với element được gọi
+}
+
+// Index History
+let indexHistory = document.getElementsByClassName("fa-history")
+indexHistory[0].addEventListener("click", function() {
+    logHistory.classList.toggle("indexHistory")
 })
